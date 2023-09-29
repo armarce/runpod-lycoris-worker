eee72f5a88514dde84ef37de1eeed6f61489d041
Armando <ikkimasi@gmail.com>
2023-09-29 02:00:33 -0400

    Create RenameImages for group images

---
+++ renameImages.py
@@ -0,0 +1,29 @@
+import os
+import pathlib
+
+def renameImages(datasetDir, train_batch_size, output_name):
+
+    images = os.listdir(datasetDir)
+
+    i = 1
+    group = 1
+
+    for image in images:
+
+        ext = pathlib.Path(image).suffix.strip().lower()
+
+        if ext != '.jpg':
+
+            raise ValueError('Only jpg images are allowed')
+
+        os.rename(f"{datasetDir}/{image}", f"{datasetDir}/group-{group}-{i}-{output_name}{ext}")  
+
+        if(i == train_batch_size):
+
+            i = 1
+
+            group = group + 1
+        
+        else:
+
+            i = i + 1
